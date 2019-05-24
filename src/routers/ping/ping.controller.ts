066c8cecb4e16a677b66761edf1b1490aafe20f8
UtopiaBeam <utopiabeam@gmail.com>
2019-05-24 11:31:16 +0700

    :recycle: Refactor ping

--- src/routers/ping/ping.controller.ts
+++ src/routers/ping/ping.controller.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { asyncifyHandler } from '../utils/async-handler';
 import pingService from './ping.service';
+import mongoose from 'mongoose';
 
 const router: Router = Router();
 
@@ -8,8 +9,11 @@ router.get('/', (req, res) => {
     return res.send(pingService.ping());
 });
 
-router.get('/db', asyncifyHandler(async (req, res) => {
-    return res.send(await pingService.pingDatabase());
-}));
+router.get(
+    '/db',
+    asyncifyHandler(async (req, res) => {
+        return res.send(await pingService.pingDatabase(mongoose));
+    }),
+);
 
 export default router;
