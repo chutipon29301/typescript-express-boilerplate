747fd9b2c13dc776c31805e88e9a71d2c271ca43
UtopiaBeam <utopiabeam@gmail.com>
2019-05-24 11:31:16 +0700

    :recycle: Refactor ping

--- src/routers/ping/ping.service.ts
+++ src/routers/ping/ping.service.ts
@@ -1,15 +1,11 @@
-import { connection } from 'mongoose';
+import { Mongoose } from 'mongoose';
 
 export default {
     ping(): object {
         return { message: 'pong!' };
     },
-    pingDatabase(): object {
-        const isReady: number = connection.readyState;
-        if (isReady === 1) {
-            return { message: 'Database is ready.' };
-        } else {
-            return { message: 'Database is not ready.' };
-        }
+    pingDatabase(mongoose: Mongoose): object {
+        const isReady: number = mongoose.connection.readyState;
+        return { ready: isReady === 1 };
     },
 };
